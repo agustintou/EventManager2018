009560cbe07025beed53f261ca204c564f38ced4
Agustin Touceda Papa <agustin_touceda@hotmail.com>
2018-12-05 01:11:30 -0300

    EventPage 50%

---
+++ EventManager2018/EventManager2018/EventManager2018/ViewModels/EventViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace EventManager2018.ViewModels
+{
+    public class EventViewModel : BaseViewModel
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Builders
+        public EventViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
--- EventManager2018/EventManager2018/EventManager2018/ViewModels/MainViewModel.cs
+++ EventManager2018/EventManager2018/EventManager2018/ViewModels/MainViewModel.cs
@@ -4,12 +4,15 @@
     {
         #region Properties
         public MenuViewModel MenuVM { get; set; }
+
+        public EventViewModel EventVM { get; set; }
         #endregion
 
         #region Builders
         public MainViewModel()
         {
             MenuVM = new MenuViewModel();
+            EventVM = new EventViewModel();
         }
         #endregion
 
